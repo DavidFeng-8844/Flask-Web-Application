d1d964bc68d16adf4d29771e1a24e819193f6391
DavidFeng-8844 <davidfeng.8844@gmail.com>
2024-10-23 23:13:47 +0800

    v4.2 add perfect working calendar again

---
+++ FlaskTodo_v4.2/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_todo.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True   
+
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'e683639d1e6795cb015ec777f155e67d'
