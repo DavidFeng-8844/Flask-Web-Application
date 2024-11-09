62d3af27b53164def3ca1526121f3cb83b9d0959
DavidFeng-8844 <davidfeng.8844@gmail.com>
2024-11-09 21:41:38 +0800

    solve all darn WCAG warnings

--- FlaskTodo/app/static/js/task-filter.js
+++ FlaskTodo/app/static/js/task-filter.js
@@ -1,8 +1,14 @@
 // static/filter_tasks.js
-function filterTasksByDeadline(selectElement) {
+function filterTasksByDeadline() {
+    const selectElement = document.getElementById('deadline-filter');
     const selectedDeadline = selectElement.value;
     
-    // Redirect to apply the selected deadline filter
-    const url = `${window.location.origin}${selectElement.dataset.url}?deadline=${selectedDeadline}`;
-    window.location.href = url;
+    // Check if a valid option is selected
+    if (selectedDeadline) {
+        // Construct the URL and redirect
+        const url = `${window.location.origin}${selectElement.dataset.url}?deadline=${selectedDeadline}`;
+        window.location.href = url;
+    } else {
+        alert('Please select a deadline filter.');
+    }
 }
