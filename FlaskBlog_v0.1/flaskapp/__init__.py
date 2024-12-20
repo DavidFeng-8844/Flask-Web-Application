5f3d85c8625c49b8766d5d37be6e22724433523a
DavidFeng-8844 <davidfeng.8844@gmail.com>
2024-12-20 20:42:32 +0800

    add backup for v0.1

---
+++ FlaskBlog_v0.1/flaskapp/__init__.py
@@ -0,0 +1,17 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask import Flask
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '2951b9d5bb58fe1fac16d872533168aa'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///flaskblog.db')
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from flaskapp import routes
---
+++ FlaskBlog_v0.1/flaskapp/forms.py
@@ -0,0 +1,113 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.fields import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError, Email
+from flaskapp.models import User
+from flask_login import current_user
+
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email',
+                    validators=[DataRequired(), Email()])
+    username = StringField(label='Username', validators=[
+        DataRequired(),
+        Length(min=2, max=20)
+    ])
+    password = PasswordField(label='Password', validators=[
+        DataRequired(),
+        Length(min=6)
+    ])
+    confirm_password = PasswordField(label='Confirm Password', validators=[
+        DataRequired(),
+        EqualTo('password')
+    ])
+    submit = SubmitField(label='Register')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username is already taken.")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email is already taken.")
+
+
+# class LoginForm(FlaskForm):
+#     username = StringField(label='Username', validators=[
+#         DataRequired(),
+#         Length(min=2, max=20)
+#     ])
+#     password = PasswordField(label='Password', validators=[
+#         DataRequired(),
+#         Length(min=6)
+#     ])
+#     remember = BooleanField(label='Remember me')
+#     submit = SubmitField(label='Login')
+
+#     def validate_username(self, username):
+#         user = User.query.filter_by(username=username.data).first()
+#         if not user:
+#             raise ValidationError("Username does not exist. Please register first.")
+
+class UsernameLoginForm(FlaskForm):
+    username = StringField(label='Username', validators=[
+        DataRequired(),
+        Length(min=2, max=20)
+    ])
+    password = PasswordField(label='Password', validators=[
+        DataRequired(),
+        Length(min=6)
+    ])
+    remember = BooleanField(label='Remember me')
+    submit = SubmitField(label='Login')
+
+
+class EmailLoginForm(FlaskForm):
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Email()
+    ])
+    password = PasswordField(label='Password', validators=[
+        DataRequired(),
+        Length(min=6)
+    ])
+    remember = BooleanField(label='Remember me')
+    submit = SubmitField(label='Login')
+
+
+
+class PostForm(FlaskForm):
+    content = TextAreaField(label='Content')
+    media = FileField(label='Upload pic', validators=[
+        FileAllowed(('jpeg', 'jpg', 'png'))
+    ])
+    submit = SubmitField(label='Post')
+
+
+
+class CommentPostForm(FlaskForm):
+    content = TextAreaField(label='Comment')
+    submit = SubmitField(label='Post')
+
+
+
+class AccountUpdateForm(FlaskForm):
+    username = StringField(label='Change Username', validators=[
+        DataRequired(),
+        Length(min=2, max=20)
+    ])
+    picture = FileField(label='Upload new profile pic', validators=[
+        FileAllowed(('jpg', 'png'))
+    ])
+    submit = SubmitField(label='Update')
+
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("Username is already take.")
